093970b4c1c5ebac74eca0fcbf2c8d350748fde8
Liam Stabeno <liamstabeno@gmail.com>
2017-03-03 13:02:53 -0800

    add cost prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
 //Back-End
-function Pizza(names, toppings, size)  {
+function Pizza(names, toppings, size, price)  {
   this.names = names
   this.toppings = toppings;
   this.size = size;
   this.price = price;
-
 }
-// var tops = [];
-// var Pizza = new Pizza(name, tops, size, price);
-
-function names(input)  {
-return"Inputed Name"
-}
-// function toppings() {}=
-
-
+Pizza.prototype.cost=function(){
+price = 0;
+price += (this.toppings.length*2);
+  if (this.size === "small"){
+    price+= 8
+  };
+  if (this.size === "medium"){
+    price+= 10
+  };
+  if (this.size === "large"){
+    price+= 12
+  };
+  if (this.size === "xl"){
+    price+= 14
+  };
+  return price;
+};
+//Front End
 $(function() {
   $("form#pizzaForm").submit(function(event){
     event.preventDefault();
@@ -24,11 +32,11 @@ $(function() {
     $("input:checkbox[name=toppings]:checked").each(function(){
       orderTops.push($(this).val());
     });
-    console.log(orderTops, orderSize, orderName);
 
-
-
-
+console.log(orderTops, orderSize, orderName);
+var newPie  = new Pizza(orderName, orderTops, orderSize);
+console.log(newPie);
+price = newPie.cost()
+console.log(price);
 });
-
 });
