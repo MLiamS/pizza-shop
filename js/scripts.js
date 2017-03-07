04cc4bcb2c6c6dcd8201144fc85857c04c42ac58
Liam Stabeno <liamstabeno@gmail.com>
2017-03-06 18:15:04 -0800

    adjust forms: start to adjust back-end

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 //Back-End
-function Pizza(name, toppings, size )  {
-  this.name = name;
+function Pizza(toppings, size )  {
   this.toppings = toppings;
   this.size = size;
 };
 
-Pizza.prototype.cost=function()  {
+Pizza.prototype.cost = function()  {
   p = 0;
   p += (this.toppings.length*2);
   if (this.size === "Small"){
@@ -22,33 +21,36 @@ Pizza.prototype.cost=function()  {
   };
   return p;
 };
-// function addPrice(p1, total) { newPrice = p1 + total;
-//   return newPrice;
-// };
-//Front End
+
+
 $(function() {
   var totalPrice = 0;
   $("form#pizzaForm").submit(function(event){
     event.preventDefault();
     var orderTops = [];
-    var orderName = $("input#orderName").val();
     var orderSize = $("input:radio[name=size]:checked").val();
     $("input:checkbox[name=toppings]:checked").each(function(){
       orderTops.push($(this).val());
     });
 
-    var newPie  = new Pizza(orderName, orderTops, orderSize);
+    var newPie  = new Pizza(orderTops, orderSize);
     price = newPie.cost();
-    $("#order").append("<li>" + "Order for:  " + newPie.name + "</li>");
-    $("#order").append("<li>" +"Size: " + newPie.size + "</li>");
-    $("#order").append("<li>" + "Toppings: " + newPie.toppings + "</li>");
-    $("#order").append("<li>" + "Price:  $" + price + "'s" +"</li>");
-    $("#order").append("<li>" + "</li>");
-    this.("pizzaForm").reset();
+    // $("#order").append("<li>" + "Order for:  " + newPie.name + "</li>");
+    $("#order").append("<li>" +"One " + newPie.size + "</li>");
+    $("#order").append("<li>" + "With: " + newPie.toppings + "</li>");
+    $("#order").append("<li>" + "At  $" + price + "'s" +"</li>"+"<br>");
+    totalPrice = (totalPrice + price)
+    console.log(totalPrice);
+    document.getElementById("pizzaForm").reset();
+
+  });
+
+  var orderName = $("input#orderName").val();
+
+
     // addPrice(price, totalPrice);
     // console.log(price, totalPrice);
     // console.log(newPrice);
-  });
   // $("#place").click(function(){
   //   $("#items").text("items");
   //   $("#receipt").modal();
